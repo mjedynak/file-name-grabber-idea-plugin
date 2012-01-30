50eee6fd61f8dbdb55b649f8c4001a042a4dff05
Michal Jedynak <m.jedynak@gmail.com>
2012-01-30 22:18:56 +0100

    refactoring

--- src/test/groovy/pl/mjedynak/idea/plugins/CopyFileNameToClipboardActionTest.groovy
+++ src/test/groovy/pl/mjedynak/idea/plugins/CopyFileNameToClipboardActionTest.groovy
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.vfs.VirtualFile
+import java.awt.datatransfer.StringSelection
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -12,15 +13,14 @@ import static org.mockito.BDDMockito.given
 import static org.mockito.Mockito.mock
 import static org.mockito.Mockito.verify
 import static org.mockito.BDDMockito.*
-import java.awt.datatransfer.StringSelection
 
 @RunWith(MockitoJUnitRunner)
 class CopyFileNameToClipboardActionTest extends GroovyTestCase {
 
     CopyFileNameToClipboardAction action
 
-    @Mock
-    AnActionEvent event
+    @Mock AnActionEvent event
+    @Mock VirtualFile file
 
     @Test
     void copies_file_name_when_file_exists() {
@@ -32,14 +32,14 @@ class CopyFileNameToClipboardActionTest extends GroovyTestCase {
                 copyPasteManager
             }
         }
-        VirtualFile file = mock(VirtualFile)
 
-        given(event.getData(PlatformDataKeys.VIRTUAL_FILE)).willReturn(file)
+
+        given(event.getData(PlatformDataKeys.VIRTUAL_FILE)).willReturn file
 
         // when
-        action.actionPerformed(event)
-        
+        action.actionPerformed event
+
         // then
-        verify(copyPasteManager).setContents(any(StringSelection))
+        verify(copyPasteManager).setContents any(StringSelection)
     }
 }
